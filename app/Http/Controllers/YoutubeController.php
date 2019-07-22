dc9cd68fd0ffe68f510ec3e488ef03e1a3602dc2
angad128 <ango1993a@gmail.com>
2019-07-22 23:56:40 +0545

    Route and controller created for api

---
+++ app/Http/Controllers/YoutubeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class YoutubeController extends Controller
+{
+    public function index() {
+    	echo "Index API from index function of youtubecontroller";
+    }
+}
